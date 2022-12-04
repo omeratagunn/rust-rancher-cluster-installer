60602e3fb10db536bdc584876100b73230e5725d
Ömer Atagün <omratagn@gmail.com>
2022-12-04 14:48:09 +0100

    clean up this, will learn later

--- .history/src/core/config_20221204144551.rs
+++ .history/src/core/config_20221204144551.rs
@@ -23,7 +23,23 @@ pub(crate) fn parse_yaml_config(path_to_yaml: String) -> Vec<Yaml> {
     let mut contents = String::new();
     let _content = file.read_to_string(&mut contents);
     let docs: Vec<Yaml> = YamlLoader::load_from_str(&mut contents).unwrap();
-    
+    let fuckever = &docs[0]["masters"];
+    let mut somevec: Vec<ServerConfigurationArgs> = Vec::new();
+
+    let som = &fuckever["server1"][0]["name"].into_string().expect("move fuckever");
+    dbg!({ "" }, &fuckever["server1"][0]["name"]);
+    let server_config: ServerConfigurationArgs = ServerConfigurationArgs{
+        name: som.to_string().to_owned(),
+        ip: "fuck".parse().unwrap(),
+        username: "fuck".parse().unwrap(),
+        password: "fuck".parse().unwrap(),
+    };
+    somevec.push(server_config);
+    let yaml_input:YamlInput = YamlInput{
+        masters: somevec,
+        nodes: vec![],
+    };
+    dbg!("{}", &yaml_input.masters[0].name);
     return docs;
 
 }
