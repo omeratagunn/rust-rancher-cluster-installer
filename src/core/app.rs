923db53a1c67d151f3f90c3b559913195bff9dc3
Ömer Atagün <omratagn@gmail.com>
2022-12-07 08:45:45 +0100

    add installation for nodes

--- src/core/app.rs
+++ src/core/app.rs
@@ -1,5 +1,7 @@
+use std::fs;
+use std::path::Path;
 use colored::Colorize;
-use crate::app::yaml::parse_yaml_config;
+use crate::app::yaml::{Config, parse_yaml_config};
 
 #[path = "../utils/measurable.rs"]
 mod measure;
@@ -25,8 +27,14 @@ pub(crate) fn app( path: &String, k3s_version: &String) {
     let parsed_yaml = parse_yaml_config(path);
     spinner_handle.done();
 
-    for (master_node_index,masters) in parsed_yaml.masters.iter().enumerate() {
-        let spinner_handle = spinner::spinner(format!("{}{}{}{}", "Connecting to server: ".blue().bold(), masters.ip, " | Name: ", masters.name).parse().expect("spinner working"));
+    build_masters(&parsed_yaml, k3s_version);
+    build_nodes(&parsed_yaml, k3s_version);
+
+}
+
+fn build_masters(masters: &Config, k3s_version: &String){
+    for (master_node_index,masters) in masters.masters.iter().enumerate() {
+        let spinner_handle = spinner::spinner(format!("{}{}{}{}", "Connecting to master server: ".blue().bold(), masters.ip, " | Name: ", masters.name).parse().expect("spinner working"));
         let ssh_session = ssh::connect_server_via_ssh(&masters);
         spinner_handle.done();
         let ip = &masters.ip;
@@ -36,10 +44,45 @@ pub(crate) fn app( path: &String, k3s_version: &String) {
         }
 
         if master_node_index == 0 {
-            let command = r#"curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.23.13+k3s1" sh -s - server --cluster-init"#;
+            let mut k3s_flag = String::new();
+            k3s_flag.push_str("curl -sfL https://get.k3s.io |");
+            k3s_flag.push_str(" INSTALL_K3S_VERSION=");
+            k3s_flag.push_str(k3s_version);
+            k3s_flag.push_str(" sh -s - server --cluster-init");
+            let command =k3s_flag;
             install::install_rancher(&ssh_session, command.parse().unwrap(), "Rancher master");
             install::get_kube_config_into_local(ip, &ssh_session);
             install::get_k3s_token_and_save( &ssh_session);
+        }
+
+    }
+}
+
+fn build_nodes(masters: &Config, k3s_version: &String) {
+
+    let master_k3s_server = &masters.masters[0].ip.replace(":22", ":6443");
+    for (master_node_index,masters) in masters.nodes.iter().enumerate() {
+        let spinner_handle = spinner::spinner(format!("{}{}{}{}", "Connecting to node server: ".blue().bold(), masters.ip, " | Name: ", masters.name).parse().expect("spinner working"));
+        let ssh_session = ssh::connect_server_via_ssh(&masters);
+        spinner_handle.done();
+        let ip = &masters.ip;
+
+        let mut k3s_flag = String::new();
+        k3s_flag.push_str("curl -sfL https://get.k3s.io |");
+        k3s_flag.push_str(" INSTALL_K3S_VERSION=");
+        k3s_flag.push_str(k3s_version);
+        k3s_flag.push_str(" K3S_URL=https://");
+        k3s_flag.push_str(master_k3s_server);
+        k3s_flag.push_str(" K3S_TOKEN=");
+        let mut token = fs::read_to_string("kubeconfig/token").expect("should have been read the file");
+        sanitize::strip_trailing_nl(&mut token);
+        k3s_flag.push_str(&token);
+        k3s_flag.push_str(" sh -");
+        let command =k3s_flag;
+
+        install::install_rancher(&ssh_session, command.parse().unwrap(), "Rancher worker");
+        for (i, instructions) in installation::get_installation().linux_amd64.iter().enumerate() {
+            install::install_common(instructions, &ssh_session);
         }
 
     }
--- src/utils/sanitize.rs
+++ src/utils/sanitize.rs
@@ -1,10 +1,10 @@
-// pub(crate) fn strip_trailing_nl(input: &mut String) {
-//     let new_len = input
-//         .char_indices()
-//         .rev()
-//         .find(|(_, c)| !matches!(c, '\n' | '\r'))
-//         .map_or(0, |(i, _)| i + 1);
-//     if new_len != input.len() {
-//         input.truncate(new_len);
-//     }
-// }
+pub(crate) fn strip_trailing_nl(input: &mut String) {
+    let new_len = input
+        .char_indices()
+        .rev()
+        .find(|(_, c)| !matches!(c, '\n' | '\r'))
+        .map_or(0, |(i, _)| i + 1);
+    if new_len != input.len() {
+        input.truncate(new_len);
+    }
+}
